6cc761a010d2617c605f7fb75e046f563ee9f063
D4RKOexe <hayatotatsuyaalok@gmail.com>
2026-06-13 18:09:33 -0500

    Actualizar rpg-monthly.js

--- plugins/rpg-monthly.js
+++ plugins/rpg-monthly.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   let who = m.sender
   let user = global.db.data.users[who]
   if (!user) {
-    global.db.data.users[who] = { diamantes: 0, lastMonthly: 0 }
+    global.db.data.users[who] = { darkcoin0s: 0, lastMonthly: 0 }
     user = global.db.data.users[who]
   }
 
@@ -18,17 +18,17 @@ let handler = async (m, { conn }) => {
     let dias = Math.floor(restante / 86400000)
     let horas = Math.floor((restante % 86400000) / 3600000)
     return conn.sendMessage(m.chat, {
-      text: '𖣔 「 HINATA MONTHLY 」 ˚ʚ♡ɞ˚\n\n💫 » Ya reclamaste\n⏳ » Vuelve en ' + dias + 'd ' + horas + 'h'
+      text: '⚡ 「 DARK MONTHLY 」 ˚⚡˚\n\n💫 » Ya reclamaste\n⏳ » Vuelve en ' + dias + 'd ' + horas + 'h'
     }, { quoted: m })
   }
 
   user.diamantes = (user.diamantes || 0) + 3000
   user.lastMonthly = now
 
-  let texto = '𖣔 「 HINATA MONTHLY 」 ˚ʚ♡ɞ˚\n\n'
+  let texto = '⚡ 「 DARK MONTHLY 」 ˚ʚ♡ɞ˚\n\n'
   texto += '🎁 » Recompensa mensual\n'
-  texto += '💎 » +3000 diamantes\n'
-  texto += '💰 » Total: ' + user.diamantes + ' 💎\n\n'
+  texto += '💵 » +3000 darkcoins\n'
+  texto += '💰 » Total: ' + user.darkcoins + ' 💵\n\n'
   texto += '> Vuelve en 30 días'
 
   await conn.sendMessage(m.chat, { text: texto }, { quoted: m })
@@ -37,6 +37,6 @@ let handler = async (m, { conn }) => {
 handler.help = ['monthly']
 handler.tags = ['rpg']
 handler.command = /^(monthly|mensual|don2)$/i
-handler.desc = 'Recompensa mensual de 3000 💎'
+handler.desc = 'Recompensa mensual de 3000 💵 '
 
 export default handler
