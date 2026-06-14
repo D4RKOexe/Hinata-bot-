841f11e1470573493cc7a3911597308e90b19dfe
D4RKOexe <hayatotatsuyaalok@gmail.com>
2026-06-14 17:46:20 -0500

    Actualizar owner-addiamantes.js

--- plugins/owner-addiamantes.js
+++ plugins/owner-addiamantes.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
 
   if (!owners.includes(who)) {
     return conn.sendMessage(m.chat, {
-      text: '💎 「 DARKO DAR DIAMANTES 」 💎\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦\n\n💫 » Solo los creadores pueden usar esto\n\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦'
+      text: '💎 「 DARKO DAR DARKCOINS 」 💎\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦\n\n💫 » Solo los creadores pueden usar esto\n\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦'
     }, { quoted: m })
   }
 
@@ -13,28 +13,28 @@ let handler = async (m, { conn, args }) => {
 
   if (isNaN(cantidad) || cantidad <= 0) {
     return conn.sendMessage(m.chat, {
-      text: '💎 「 DARKO DAR DIAMANTES 」 💎\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦\n\n💫 » Cantidad inválida\n\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦\n> #dardiamantes 100\n> #dardiamantes @usuario 100'
+      text: '💎 「 DARKO DAR DARKCOINS 」 💎\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦\n\n💫 » Cantidad inválida\n\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦\n> #dardarkcoins 100\n> #dardarkcoins @usuario 100'
     }, { quoted: m })
   }
 
   let user = global.db.data.users[target]
   if (!user) {
-    global.db.data.users[target] = { diamantes: 0, bank: 0, exp: 0, level: 0 }
+    global.db.data.users[target] = { darkcoins: 0, bank: 0, exp: 0, level: 0 }
     user = global.db.data.users[target]
   }
 
-  user.diamantes = (user.diamantes || 0) + cantidad
+  user.darkcoins = (user.darkcoins || 0) + cantidad
   global.markDatabaseModified()
 
   await conn.sendMessage(m.chat, {
-    text: '💎 「 DARKO DAR DIAMANTES 」 💎\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦\n\n💫 » Diamantes entregados\n\n👤 » @' + target.split('@')[0] + '\n💎 » +' + cantidad + ' diamantes\n💰 » Total: ' + user.diamantes + ' 💎\n\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦',
+    text: '💎 「 DARKO DAR DARKCOINS 」 💎\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦\n\n💫 » Darkcoins entregados\n\n👤 » @' + target.split('@')[0] + '\n💵 » +' + cantidad + ' darkcoins\n💰 » Total: ' + user.darkcoins + ' 💵\n\n✦•┈๑⋅⋯ ⋯⋅๑┈•✦',
     mentions: [target]
   }, { quoted: m })
 }
 
-handler.help = ['dardiamantes']
+handler.help = ['dardarkcoins']
 handler.tags = ['owner']
-handler.command = /^(dardiamantes|dardinero|adddiamantes)$/i
-handler.desc = 'Da diamantes a un usuario'
+handler.command = /^(dardarkcoinss|dardinero|adddiamantes)$/i
+handler.desc = 'Da darkcoins a un usuario'
 
 export default handler
